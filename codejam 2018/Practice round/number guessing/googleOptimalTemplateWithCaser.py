135e5c34d9b3a2cd1b378e7a97eb4d59e0bddd05
Robert Kristian Alm <exarchiasghost@gmail.com>
2018-04-01 03:18:43 +0200

    Strange bug
    something visious are going on with my code. My program works perfectly with small ranges but it ignores MAX aon small ranges. I suspect bug on my IDE or something..
    I will check it tomorow.

--- codejam 2018/Practice round/number guessing/googleOptimalTemplateWithCaser.py
+++ codejam 2018/Practice round/number guessing/googleOptimalTemplateWithCaser.py
@@ -1,5 +1,15 @@
+import random
+
 itIsTheFirstLine = True     #This variable is about the input line. If the line is the first the variable itIsTheFirstLine is True.
 T=0     #The number of lines with usefull Data. the number of tierations.
+theRangeArray = []
+A=""
+B=""
+newA=0
+newN=0
+lastNumber=0
+lastMAX=0
+lastMIN=0
 result = "Not any result right now" #this is used for the result wich is printed in the end of the code with the default value.
 testSum = 0 #This is used to make the mainCheckFunction() bit more interesting but making the function to work as add function.
 countForT = 0 #this is the count vairiable that helps to check that the iterations are not more than the predefined T variable.
@@ -13,17 +23,57 @@ working = True
 #fileForInput = input("feed me with input")
 
 
+def setNewMax(newMAX, oldMAX):
+    if newMAX < oldMAX:
+        return newMAX
+    else:
+        return oldMAX
+
+def setNewMin(newMIN, oldMIN):
+    if newMIN > oldMIN:
+        return newMIN
+    else:
+        return oldMIN
 
 
 def guessNumber(answerFromTheSystem):
+    global i, newA, newB, lastNumber, lastMAX, lastMIN
     if answerFromTheSystem == "TOO_SMALL":
         print("the number is too small")
+        lastMIN = setNewMin(lastNumber, lastMIN)
+        lastNumber = smallGuess(lastMIN, lastMAX)
+        print("I recommend", lastNumber)
     elif answerFromTheSystem == "TOO_BIG":
         print("the number is too big")
+        lastMAX = setNewMax(lastNumber, lastMAX)
+        lastNumber = bigGuess(lastMIN, lastMAX)
+        print("I recommend", lastNumber)
     elif answerFromTheSystem == "CORRECT":
         print("that is the correct number")
+        newA = 0
+        newB = 0
+        i = int(T)
     else:
         print("Something is wrong")
+
+
+def firstGuess(A,B):
+    theNumber = random.randrange(int(A), int(B), 1)
+    print("I am doing a firstGuess. MIN is ", A, " and MAX is", B)
+    return str(theNumber)
+
+def smallGuess(A,B):
+    theNumber = random.randrange(int(A)+1, int(B), 1)
+    print("I am doing a smallGuess. MIN is ", A, " and MAX is", B)
+    return str(theNumber)
+
+def bigGuess(A,B):
+    theNumber = random.randrange(int(A), int(B)-1, 1)
+    print("I am doing a bigGuess. MIN is ", A, " and MAX is", B)
+    return str(theNumber)
+
+
+
 
 
 while (working == True):
@@ -33,6 +83,22 @@ while (working == True):
         itIsTheFirstLine = False
         print("it is the first line")
     else:
+        theRange = input("define the range [A B]")
+        theRangeArray = theRange.split()
+        A = theRangeArray[0]
+        B = theRangeArray[1]
+        newA = theRangeArray[0]
+        newB = theRangeArray[1]
+        print("The theRangeArray is", theRangeArray)
+        print("The range is", theRange)
+        print("The A is", A)
+        print("The B is", B)
+        theFirstChoosenNumber = firstGuess(A,B)
+        lastNumber = theFirstChoosenNumber
+        lastMAX = B
+        lastMIN = A
+        print("The first choosen number is", theFirstChoosenNumber)
+
         print("Now we check the cases")
         while i < int(T):
             fileForInput = input("give the second input input")
